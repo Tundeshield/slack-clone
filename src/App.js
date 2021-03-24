eebaef463ce495e63ac55aa256dc50f2a78dcc4f
Tunde Adepegba <tundeshield@gmail.com>
2021-03-24 13:41:26 +0100

    login and authentication completed.

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ import Header from "./components/Header";
 import styled from "styled-components";
 import SideBar from "./components/SideBar";
 import Chat from "./components/Chat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
 
 function App() {
+	const [user, loading] = useAuthState(auth);
+
 	return (
 		<div className="App">
 			<Router>
-				<>
-					<Header />
-					<AppBody>
-						<SideBar />
-						<Switch>
-							<Route path="/" exact>
-								<Chat/>
-								{/*chat*/}
-							</Route>
-						</Switch>
-					</AppBody>
-				</>
+				{!user ? (
+					<Login/>
+				) : (
+					<>
+						<Header />
+						<AppBody>
+							<SideBar />
+							<Switch>
+								<Route path="/" exact>
+									<Chat />
+									{/*chat*/}
+								</Route>
+							</Switch>
+						</AppBody>
+					</>
+				)}
 			</Router>
 		</div>
 	);
